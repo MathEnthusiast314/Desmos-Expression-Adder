de690c284e9bbffb1d713bcf0cb9b0e86db79b76
MathEnthusiast314 <89387162+MathEnthusiast314@users.noreply.github.com>
2021-08-31 21:31:28 +0530

    Create note-expression.js

---
+++ note-expression.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name        Desmos enter expressions using latex
+// @namespace   Scripts
+// @match       *://www.desmos.com/calculator*
+// @grant       none
+// @version     1.0
+// @run-at      document-start
+// @author      -
+// ==/UserScript==
+
+var tid = setInterval(mycode, 500);
+function mycode() {
+state = Calc.getState()
+state.expressions.list.forEach(a=>{
+if (a.text){
+if (a.text.substring(0,5)=="latex"){
+  a2 = state.expressions.list[-1+Number((a.text.substring(5,a.text.indexOf("\n"))))];
+  a2.latex = a.text.substring(a.text.indexOf("\n")+1,a.text.length)
+}
+Calc.setExpression(a2)
+}})
+}
+/*
+function abortTimer() { // to be called when you want to stop the timer
+  clearInterval(tid);
+}*/
